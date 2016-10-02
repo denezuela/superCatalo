104c385b17c201860088d316c60cac9b0ffd4435
denezuela <zagumionnov.denis@yandex.ru>
2016-10-02 22:25:04 +0300

    add data structure Node for model

--- superCatalo/imageprovider.h
+++ superCatalo/imageprovider.h
@@ -1,1 +1,36 @@
+#ifndef IMAGEPROVIDER_H
+#define IMAGEPROVIDER_H
 
+#include <QObject>
+#include <QAbstractItemModel>
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QString>
+
+class ImageProvider : public QAbstractItemModel
+{
+    Q_OBJECT
+
+    struct Node {
+        qint64 id;
+        qint64 pid;
+        QString path;
+        QString text;
+        QString comments;
+        QString tags;
+    };
+
+public:
+    explicit ImageProvider(QAbstractItemModel *parent = 0);
+    virtual QVariant data(const QModelIndex &index, int role) const;
+    virtual QModelIndex index(int row, int column, const QModelIndex &index) const;
+    virtual int rowCount(const QModelIndex &index) const;
+    virtual int columnCount(const QModelIndex &index) const;
+    virtual QModelIndex parent(const QModelIndex &index) const;
+
+signals:
+
+public slots:
+};
+
+#endif // IMAGEPROVIDER_H
